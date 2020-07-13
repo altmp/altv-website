5918bf78e943b6814b1cfa5ba57f3b75478b7c97
Hazard <hazard9909@gmail.com>
2020-07-13 22:54:16 +0200

    Fixed public path in webpack

--- configs/webpack.common.js
+++ configs/webpack.common.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: path.resolve(__dirname, '../src/main.js'),
   output: {
     path: path.resolve(__dirname, '../public'),
-    publicPath: '/',
+    // publicPath: '/',
     filename: 'js/[name].[hash].js'
   },
   module: {
