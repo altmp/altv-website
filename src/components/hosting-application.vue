63caa9e731e9b08c1974621b150abb8948117bda
Trevor Wessel <stuykgaming@gmail.com>
2020-07-13 18:07:26 -0600

    Prettier Notation

--- src/components/hosting-application.vue
+++ src/components/hosting-application.vue
@@ -1,28 +1,26 @@
 <template>
-  <div class="main">
-    <iframe
-      src="https://docs.google.com/forms/d/e/1FAIpQLSfDiGR7aKSZGCvf6BMZfBgSIyIOWxZaVRm2qi57adRJerXruA/viewform?embedded=true"
-      width="640"
-      height="969"
-      frameborder="0"
-      marginheight="0"
-      marginwidth="0"
-      style="background: rgb(236, 240, 234); border-radius: 10px;"
-    >
-      Загрузка…
-    </iframe>
-  </div>
+    <div class="main">
+        <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLSfDiGR7aKSZGCvf6BMZfBgSIyIOWxZaVRm2qi57adRJerXruA/viewform?embedded=true"
+            width="640"
+            height="969"
+            frameborder="0"
+            marginheight="0"
+            marginwidth="0"
+            style="background: rgb(236, 240, 234); border-radius: 10px;"
+        >Загрузка…</iframe>
+    </div>
 </template>
 
 <script>
-export default { }
+export default {};
 </script>
 
 <style scoped>
-  .main {
+.main {
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     align-items: center;
-  }
+}
 </style>
--- src/components/report-tos.vue
+++ src/components/report-tos.vue
@@ -1,28 +1,26 @@
 <template>
-  <div class="main">
-    <iframe
-      src="https://docs.google.com/forms/d/e/1FAIpQLSfT-J-H76N52drXqinpE6LIVNd2qaHBuAWiYsC1b4sUjW_jTA/viewform?embedded=true"
-      width="640"
-      height="969"
-      frameborder="0"
-      marginheight="0"
-      marginwidth="0"
-      style="background: rgb(236, 240, 234); border-radius: 10px;"
-    >
-      Загрузка…
-    </iframe>
-  </div>
+    <div class="main">
+        <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLSfT-J-H76N52drXqinpE6LIVNd2qaHBuAWiYsC1b4sUjW_jTA/viewform?embedded=true"
+            width="640"
+            height="969"
+            frameborder="0"
+            marginheight="0"
+            marginwidth="0"
+            style="background: rgb(236, 240, 234); border-radius: 10px;"
+        >Загрузка…</iframe>
+    </div>
 </template>
 
 <script>
-export default { }
+export default {};
 </script>
 
 <style scoped>
-  .main {
+.main {
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     align-items: center;
-  }
+}
 </style>
