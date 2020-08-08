f02f364fbaf3b800aab40266ed674dedc2fbfe9f
iamfronty <me@fronty.ws>
2020-08-08 13:21:47 +0000

    Deploying to gh-pages from  @ edf10ed40d5635dabed98c9437710b54260ac930 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1d3007f3bceb53fb7fde6b5de748e3ea.js"
+  "/precache-manifest.509852a4b6592e826a7928628516622c.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "altv-website"});
